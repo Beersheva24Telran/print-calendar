ed0571b6c4bbd2f41bb30f1814cd6c8833c3c196
Yuragranovsky <yuragranovsky@gmail.com>
2024-08-15 10:48:22 +0300

    HW #17 fixed first day - Monday

--- src/main/java/telran/time/Main.java
+++ src/main/java/telran/time/Main.java
@@ -1,57 +1,140 @@
 package telran.time;
 
 import java.time.DayOfWeek;
-record MonthYear(int month, int year) {}
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.YearMonth;
+import java.time.format.TextStyle;
+import java.time.temporal.ChronoField;
+import java.util.Locale;
+
+record MonthYear(int month, int year) {
+	
+}
 public class Main {
-    public static void main(String[] args) {
-        try {
-            MonthYear monthYear = getMonthYear(args); //if no arguments current year and month should be applied
-            printCalendar(monthYear);
-        } catch (RuntimeException e) {
-                e.printStackTrace();
-        }
-        catch (Exception e) {
-           System.out.println(e.getMessage());
-        }
-       
-    }
 
-    private static void printCalendar(MonthYear monthYear) {
-        printTitle(monthYear);
-        printWeekDays();
-        printDates(monthYear);
-    }
+	private static final int TITLE_OFFSET = 5;
+	private static final int COLUMN_WIDTH = 4;
+	private static DayOfWeek[] weekDays = DayOfWeek.values();
+	public static void main(String[] args)  {
+		try {
+			MonthYear monthYear = getMonthYear(args);
+			printCalendar(monthYear);
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 
-    private static void printDates(MonthYear monthYear) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'printDates'");
-    }
+	private static  MonthYear getMonthYear(String[] args) throws Exception{
+		int monthNumber = getMonth(args);
+		int year = getYear(args);
+		return new MonthYear(monthNumber, year);
+	}
 
-    private static void printWeekDays() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'printWeekDays'");
-    }
+	private static int getYear(String[] args) throws Exception {
+		int year = args.length < 2 ? getCurrentYear() : getYear(args[1]);
+		return year;
+	}
 
-    private static void printTitle(MonthYear monthYear) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'printTitle'");
-    }
+	private static int getYear(String yearStr) throws Exception {
+		try {
+			int res = Integer.parseInt(yearStr);
+			return res;
+		} catch (NumberFormatException e) {
+			throw new Exception("year must be an integer number");
+		}
+		
+	}
 
-    private static MonthYear getMonthYear (String[] args) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getMonthYear'");
-    }
-    private int getFirstDayofWeek(MonthYear monthYear) {
-        //TODO
-        return -1;
-    }
-    private int getOffset(int firstWeekDay) {
-        //TODO
-        //shift on this offset for starting printin
-        return -1;
-    }
-    private int getLastDayOfMonth(MonthYear monthYear) {
-        //TODO
-        return -1;
-    }
+	private static int getCurrentYear() {
+		
+		return LocalDate.now().getYear();
+	}
+
+	private static int getMonth(String[] args) throws Exception{
+		int month = args.length == 0 ? getCurrentMonth() : getMonthNumber(args[0]);
+		return month;
+	}
+
+	private static int getMonthNumber(String monthStr)throws Exception {
+		try {
+			int result = Integer.parseInt(monthStr);
+			if (result < 1) {
+				throw new Exception("Month cannot be less than 1");
+			}
+			if(result > 12) {
+				throw new Exception("Month cannot be greater than 12");
+			}
+			return result;
+		} catch (NumberFormatException e) {
+			throw new Exception("Month must be a number");
+		}
+	}
+
+	private static int getCurrentMonth() {
+		
+		return LocalDate.now().get(ChronoField.MONTH_OF_YEAR);
+	}
+
+	private static void printCalendar(MonthYear monthYear) {
+		printTitle(monthYear);
+		printWeekDays();
+		printDates(monthYear);
+		
+		
+	}
+
+	private static void printDates(MonthYear monthYear) {
+		int lastDayOfMonth = getLastDayOfMonth(monthYear);
+		int currentWeekDay = getFirstDayOfWeek(monthYear);
+		int firstOffset = getOffset(currentWeekDay);
+		System.out.printf("%s", " ".repeat(firstOffset));
+		for(int day = 1; day <= lastDayOfMonth; day++) {
+			System.out.printf("%" + COLUMN_WIDTH +"d", day);
+			if(currentWeekDay == weekDays.length) {
+				currentWeekDay = 0;
+				System.out.println();
+			}
+			currentWeekDay++;
+		}
+		
+	}
+
+	private static int getOffset(int currentWeekDay) {
+		
+		return COLUMN_WIDTH * (currentWeekDay - 1);
+	}
+
+	private static int getFirstDayOfWeek(MonthYear monthYear) {
+		LocalDate ld = LocalDate.of(monthYear.year(), monthYear.month(),
+				1);
+		return ld.get(ChronoField.DAY_OF_WEEK);
+	}
+
+	private static int getLastDayOfMonth(MonthYear monthYear) {
+		YearMonth ym = YearMonth.of(monthYear.year(), monthYear.month());
+		return ym.lengthOfMonth();
+	}
+
+	private static void printWeekDays() {
+		System.out.printf("%s", " ".repeat(1));
+		for(DayOfWeek weekday: weekDays) {
+			System.out.printf("%" + COLUMN_WIDTH +"s",weekday.getDisplayName(TextStyle.SHORT,
+					Locale.forLanguageTag("en")));
+			
+		}
+		System.out.println();
+		
+	}
+
+	private static void printTitle(MonthYear monthYear) {
+		String monthName = Month.of(monthYear.month())
+		.getDisplayName(TextStyle.FULL, Locale.getDefault());
+System.out.printf("\n%s%s %d\n"," ".repeat(TITLE_OFFSET), monthName, monthYear.year());
+	}
+    
+   
+    
 }
